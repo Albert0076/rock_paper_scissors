fa3adbe8816a8a647cacefda68cc8ab2313a2e8c
Albert0076 <albain0809@highgateschool.org.uk>
2023-12-10 15:57:29 +0000

    Added game object

--- rpc_back.py
+++ rpc_back.py
@@ -76,3 +76,74 @@ class Game:
         self.round_result = None
         self.round_winner = None
 
+    def add_human_player(self, name):
+        self.players.append(HumanPlayer(name))
+
+    def add_computer_player(self):
+        self.players.append(ComputerPlayer("Computer"))
+
+    def set_max_rounds(self, rounds):
+        if isinstance(rounds, int):
+            self.max_rounds = rounds
+
+    def find_winner(self):
+        player_1 = self.players[0]
+        player_2 = self.players[1]
+        if player_1.current_object is not None and player_2.current_object is not None:
+            if player_1.current_object == player_2.current_object:
+                self.round_result = "Draw"
+
+            elif player_1.current_object > player_2.current_object:
+                self.round_winner = player_1.name
+                player_1.win_round()
+                self.round_result = "Won"
+            else:
+                self.round_winner = player_2.name
+                player_2.win_wound()
+                self.round_result = "Won"
+
+    def next_round(self):
+        self.current_round += 1
+        self.round_result = None
+        self.round_winner = None
+        for player in self.players:
+            player.reset_object()
+
+    def is_finished(self):
+        return self.current_round >= self.max_rounds
+
+    def reset(self):
+        self.current_round = 0
+        for player in self.players:
+            player.score = 0
+
+    def report_round(self):
+        player_1 = self.players[0]
+        player_2 = self.players[1]
+        message = (f"{player_1.name} chose {player_1.current_object}.\n"
+                   f"{player_2.name} chose {player_2.current_object}.\n"
+                   f"The round resulted in a {self.round_result}.\n")
+
+        if self.round_winner == player_1.name:
+            message += f"{player_1.name} won the round.\n"
+
+        elif self.round_winner == player_2.name:
+            message += f"{player_2.name} won the round.\n"  #
+
+        return message
+
+    def report_score(self):
+        return (f"{self.players[0].name} has {self.players[0].score} points.\n"
+                f"{self.players[1].name} has {self.players[1].score} points")
+
+    def report_winner(self):
+        message = ""
+        if self.players[0].score == self.players[1].score:
+            message = f"The game is a draw."
+
+        elif self.players[0].score > self.players[1].score:
+            message = f"{self.players[0].name} wins the game"
+
+        else:
+            message = f"{self.players[1].name} wins the game"
+
